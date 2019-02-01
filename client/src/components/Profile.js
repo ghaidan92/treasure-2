f6f734adc2fc032286c8812b6049256a8f6935ef
Alexander Mutuc <AlexMutuc@Alexanders-MacBook-Pro.local>
2019-02-01 10:08:05 -0800

    items on front-end/user based

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -13,24 +13,20 @@ class Profile extends Component {
   };
 
   componentDidMount() {
-
     API.getUser(this.props.user.id)
     .then(res => {
-      
       this.setState({
         username: res.data.username,
         email: res.data.email,
         userId: res.data._id,
         items: res.data.items
-      })
+      })  
     });
-  
   }
 
-  
 
   render() {
-    /*========= THIS IS ABLE TO GRAB ITEM INFORMATION BASED ON USER STATE BUT NEED TO FIND WAY TO NOT HAVE ALL FUNCTIONALITY IN RENDER METHOD. GOAL IS TO LOOP THROUGH STATE AND MAKE DYNAMIC API CALLS PLUGGING IN EACH ELEMENT OF ITEMS ARRAY INTO FUNCTION PARAMETERS*/
+    /*========= THIS IS ABLE TO GRAB ITEM INFORMATION BASED ON USER STATE BUT NEED TO FIND WAY TO NOT HAVE ALL FUNCTIONALITY IN RENDER METHOD. GOAL IS TO LOOP THROUGH STATE AND MAKE DYNAMIC API CALLS PLUGGING IN EACH ELEMENT OF ITEMS ARRAY INTO FUNCTION PARAMETERS============================*/
     for (var i= 0; i < this.state.items.length; i++) {
       console.log(i)
       API.getItem(this.state.items[i]).then(res => {
